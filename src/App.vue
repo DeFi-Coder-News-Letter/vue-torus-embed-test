438534f7d99ea7b9f26e60a107fbb4f174f899fb
chaitanyapotti <chaitanya.potti@gmail.com>
2019-08-04 00:20:55 +0530

    Hello world

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <button @click="login">Login</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Torus from "@toruslabs/torus-embed";
+import Web3 from "web3";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  methods: {
+    async login() {
+      try {
+        const torus = new Torus();
+        window.torus = torus;
+        await torus.init("development");
+        await torus.ethereum.enable();
+        const web3 = new Web3(torus.provider);
+        web3.eth.getAccounts().then(accounts => {
+          web3.eth.getBalance(accounts[0]).then(console.log)
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
