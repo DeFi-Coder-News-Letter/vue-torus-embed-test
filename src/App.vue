79464fd051ac26fcc89c4c49cd2277a044cc0907
chaitanyapotti <chaitanya.potti@gmail.com>
2019-08-07 17:25:29 +0800

    new features

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <button @click="login">Login</button>
+    <button v-if="publicAddress === ''" @click="login">Login</button>
+    <button v-if="publicAddress !== ''" @click="changeProvider">Change Provider</button>
+    <button v-if="publicAddress !== ''" @click="getUserInfo">Get User Info</button>
   </div>
 </template>
 
@@ -11,19 +13,32 @@ import Web3 from "web3";
 
 export default {
   name: "app",
+  data() {
+    return {
+      publicAddress: ''
+    }
+  },
   methods: {
     async login() {
       try {
         const torus = new Torus();
-        await torus.init();
+        await torus.init('development');
         await torus.ethereum.enable();
         const web3 = new Web3(torus.provider);
         web3.eth.getAccounts().then(accounts => {
+          this.publicAddress = accounts[0]
           web3.eth.getBalance(accounts[0]).then(console.log)
         });
+        window.torus = torus
       } catch (error) {
         console.error(error);
       }
+    },
+    changeProvider() {
+      window.torus.setProvider('rinkeby')
+    },
+    async getUserInfo() {
+      const userInfo = await window.torus.getUserInfo()
     }
   }
 };
